6018e1e3795961c50e26bd3a2f9037b085970a3d
Hasumi-Uehara <slammygirl32@gmail.com>
2020-12-20 16:59:45 +0900

    テストコードの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :nickname,   presence: true
+  validates :birth_date, presence: true
 
-  validates :nickname,           presence: true
-  validates :first_name,         presence: true
-  validates :last_name,          presence: true
-  validates :first_name_reading, presence: true
-  validates :last_name_reading,  presence: true
-  validates :birth_date,         presence: true
+  validates :password,:password_confirmation, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+
+  with_options presence: true do
+    validates :first_name, :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters." }
+    validates :first_name_reading, :last_name_reading, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters." }
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,8 +15,8 @@ RSpec.describe User, type: :model do
         expect(@user).to be_valid
       end
       it 'passwordが6文字以上であれば登録できる' do
-        @user.password = '000000'
-        @user.password_confirmation = '000000'
+        @user.password = 'abc123'
+        @user.password_confirmation = 'abc123'
         expect(@user).to be_valid
       end
     end
@@ -45,16 +45,48 @@ RSpec.describe User, type: :model do
         expect(@user.errors.full_messages).to include("Password can't be blank")
       end
       it 'passwordが5文字以下であれば登録できない' do
-        @user.password = '00000'
-        @user.password_confirmation = '00000'
+        @user.password = 'abc12'
+        @user.password_confirmation = 'abc12'
         @user.valid?
         expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
       end
       it 'passwordが存在してもpassword_confirmationが一致しないと登録できない' do
-        @user.password = '123456'
-        @user.password_confirmation = '234567'
+        @user.password = 'abc123'
+        @user.password_confirmation = 'abc456'
         @user.valid?
         expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      end
+      it 'first_nameが全角日本語でないと登録できない' do
+        @user.first_name = "FURIMA"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name is invalid. Input full-width characters.")
+      end
+      it 'last_nameが全角日本語でないと登録できない' do
+        @user.last_name = "TAROU"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name is invalid. Input full-width characters.")
+      end
+      it 'first_name_readingが全角カタカナでないと登録できない' do
+        @user.first_name_reading = "ﾌﾘﾏ"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name reading is invalid. Input full-width katakana characters.")
+      end
+      it 'last_name_readingが全角カタカナでないと登録できない' do
+        @user.last_name_reading = "ﾀﾛｳ"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name reading is invalid. Input full-width katakana characters.")
+      end
+      it 'passwordが半角英語のみだと登録できない' do
+        @user.password = "abcdef"
+        @user.password_confirmation = "abcdef"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is invalid", "Password confirmation is invalid")
+      end
+      it 'passwordが半角数字のみだと登録できない' do
+        @user.password = "123456"
+        @user.password_confirmation = "123456"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is invalid", "Password confirmation is invalid")
       end
     end
   end
