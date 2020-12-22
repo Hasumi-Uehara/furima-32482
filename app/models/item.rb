ad500fd6efaf248a580540a418b2848c2eeb1fd4
Hasumi-Uehara <slammygirl32@gmail.com>
2020-12-22 10:53:45 +0900

    モデル名とアソシエーションの見直し

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,22 +1,23 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :sales_status
+  belongs_to_active_hash :shopping_fee_status
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :scheduled_delivery
 
   belongs_to :user
   has_one :order
   has_one_attached :image
 
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :category_id
-  has_one_active_hash :sales_status_id
-  has_one_active_hash :shopping_fee_status_id
-  has_one_active_hash :prefecture_id
-  has_one_active_hash :scheduled_delivery_id
-
   with_options presence: true do
     validates :image
     validates :name
     validates :info
-    validates :price, format: { with: /\A[0-9]+\z/, message: "is invalid. Input half-width characters." }
+    validates :price, format: { with: /\A[0-9]+\z/, message: 'is invalid. Input half-width characters.' }
   end
 
-  validates :category_id, :sales_status_id, :shopping_fee_status_id, :prefecture_id, :scheduled_delivery_id, numericality: { other_than: 1 }
+  # 下記データの選択が「--」のときは保存できないようにする。
+  validates :category_id, :sales_status_id, :shopping_fee_status_id, :prefecture_id, :scheduled_delivery_id,
+            numericality: { other_than: 1 }
 end
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :item do
-    
+    image                   { Faker::LoremPixel.image }
+    name                    { Faker::Lorem.characters(40) }
+    info                    { Faker::Lorem.characters(1000) }
+    category_id             { Faker::Number.between(from: 1, to: 11) }
+    sales_status_id         { Faker::Number.between(from: 1, to: 7) }
+    shopping_fee_status_id  { Faker::Number.between(from: 1, to: 3) }
+    prefecture_id           { Faker::Number.between(from: 1, to: 48) }
+    scheduled_delivery_id   { Faker::Number.between(from: 1, to: 4) }
+    price                   { Faker::Number.between(from: 300, to: 9_999_999) }
   end
 end
