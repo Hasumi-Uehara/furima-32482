205a9362f7cfd21a4cc90191766e44973c7e592b
Hasumi-Uehara <slammygirl32@gmail.com>
2020-12-22 17:00:34 +0900

    修正（1回目）

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -74,7 +74,7 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include('Price is not a number')
       end
       it '販売価格が300円を割ると出品できない' do
-        @item.price = 200
+        @item.price = 299
         @item.valid?
         expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
       end
