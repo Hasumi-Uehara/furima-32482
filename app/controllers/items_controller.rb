431af96f5725410965c196f8362c58c70298051c
Hasumi-Uehara <slammygirl32@gmail.com>
2020-12-23 19:22:03 +0900

    【修正】items#destroyをbefore_actionにまとめる

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item,           only: [:show, :edit, :update]
+  before_action :set_item,           only: [:show, :edit, :update, :destroy]
   before_action :move_to_index,      only: [:edit, :update]
 
   def index
@@ -36,9 +36,8 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    item = Item.find(params[:id])
-    if current_user.id == item.user_id
-      item.destroy
+    if current_user.id == @item.user_id
+      @item.destroy
     end
     redirect_to action: :index
   end
