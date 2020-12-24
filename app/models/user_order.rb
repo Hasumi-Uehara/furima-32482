e077825147f3a27c385f1d88db4c268789453a74
Hasumi-Uehara <slammygirl32@gmail.com>
2020-12-24 12:19:42 +0900

    購入ページのパスの指定・表示

--- app/models/user_order.rb
+++ app/models/user_order.rb
@@ -1,6 +1,6 @@
 class UserOrder
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number
+  attr_accessor :user_id, :item_id, :order_id, :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number
 
   with_options presence: true do
     validates :city
@@ -12,7 +12,8 @@ class UserOrder
   validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
 
   def save
-    order = Order.create(user_id: current_user.id, item_id: params[:id])
+    item_id = Item.find(params[:item_id])
+    order = Order.create(user_id: current_user.id, item_id: item_id)
     Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, addresses: addresses, building: building, phone_number: phone_number, order_id: order.id)
   end
 end
