be63b54af458dbc304eecf996c3a6134ca1823ba
Hasumi-Uehara <slammygirl32@gmail.com>
2020-12-26 12:32:17 +0900

    Basic認証の設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,18 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
-  # before_action :basic_auth
 
   private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'hasmin' && password == '2222'
+    end
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up,
                                       keys: [:nickname, :first_name, :last_name, :first_name_reading, :last_name_reading,
                                              :birth_date])
   end
-
-  # def basic_auth
-  #   authenticate_or_request_with_http_basic do |username, password|
-  #     username == 'hasmin' && password == '2222'
-  #   end
-  # end
 end
